21d79cde424f8e77a9408b2a42452e8c76672273
Mars Saxman <mars@redecho.org>
2015-10-21 10:40:21 -0700

    merged the test build into the regular makefile: simpler and no need to keep a duplicate set of build rules

--- target.mk
+++ target.mk
@@ -1,5 +1,6 @@
 # Configure the build target
-ASFLAGS += -march=i686 --32 --strip-local-absolute
-CFLAGS += -march=i686 -m32 -std=c99 -ffreestanding -nostdlib -nostdinc
+ARCHFLAGS = -march=i686 -m32
+ASFLAGS += $(ARCHFLAGS) --strip-local-absolute
+CFLAGS += $(ARCHFLAGS) -std=c99 -ffreestanding -nostdlib -nostdinc
 LDFLAGS += -static -nostdlib -melf_i386
 
